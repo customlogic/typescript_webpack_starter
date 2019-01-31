8dffd51a1b97978137bc581a6e830bf623f214d5
Jeff Nusz <jeff@phntms.com>
2019-01-31 14:16:10 +1300

    updates to make including packages easier

--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,16 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
 module.exports = {
-  mode : 'development',
-  entry : './src/index.ts',
-  devtool : 'inline-source-map',
-  devServer : { contentBase : './dist' },
-  module : {
-    rules : [
-      { test : /\.tsx?$/, use : 'ts-loader', exclude : /node_modules/},
+  mode: 'development',
+  entry: './src/index.ts',
+  devtool: 'source-map',
+  devServer: { contentBase: './dist', host: '0.0.0.0' },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/},
       {
-        test : /\.scss$/,
-        use : [
+        test: /\.scss$/,
+        use: [
           'style-loader', // creates style nodes from JS strings
           'css-loader',   // translates CSS into CommonJS
           'sass-loader'   // compiles Sass to CSS, using Node Sass by default
@@ -21,11 +21,11 @@ module.exports = {
       }
     ]
   },
-  resolve : { extensions : [ '.tsx', '.ts', '.js' ] },
-  output : { filename : 'bundle.js', path : path.resolve(__dirname, 'dist') },
-  plugins : [
-    new CleanWebpackPlugin([ 'dist' ]),
-    new HtmlWebpackPlugin({ template : './src/index.html', favicon : 'src/favicon.ico' }),
-    new CopyWebpackPlugin([ { from : 'assets/**/*', to : '' } ])
+  resolve: { extensions: ['.tsx', '.ts', '.js'] },
+  output: { filename: '[name].[contenthash].js', path: path.resolve(__dirname, 'dist') },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin({ template: './src/index.html', favicon: './src/favicon.ico' }),
+    new CopyWebpackPlugin([{ from: 'assets/**/*', to: '' }])
   ]
 };
